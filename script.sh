22399ccc254c444eea5d669488c9002dcf548589
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-19 18:48:50 +0000

    test8

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ set -xeu
 
 TRAVIS_DEBIAN_TARGET_ARCH="i386"
 TRAVIS_DEBIAN_SUITE="sid"
-TRAVIS_DEBIAN_MIRROR="${TRAVIS_DEBIAN_MIRROR:-http://ftp.de.debian.org/debian/}"
+TRAVIS_DEBIAN_MIRROR="${TRAVIS_DEBIAN_MIRROR:-http://httpredir.debian.org/debian/}"
 
 
 HOST_PACKAGES="debootstrap qemu-user-static binfmt-support sbuild"
@@ -18,3 +18,7 @@ fi
 sudo apt-get install --yes --no-install-recommends ${HOST_PACKAGES}
 mkdir ${CHROOT_DIR}
 sudo debootstrap ${FOREIGN} --no-check-gpg --include=${CHROOT_PACKAGES} --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
+sudo cp /usr/bin/qemu-${TRAVIS_DEBIAN_TARGET_ARCH}-static ${CHROOT_DIR}/usr/bin/
+chroot ${CHROOT_DIR} ./debootstrap/debootstrap --second-stage
+echo "en_US.UTF-8 UTF-8" >> ${CHROOT_DIR}/etc/locale.gen
+sudo sbuild-createchroot --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${FOREIGN} --setup-only ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
