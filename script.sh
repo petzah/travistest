e62dd9dbe35450967cef05ce217591ecad1bfdad
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-20 19:22:22 +0000

    test35

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ FOREIGN="${FOREIGN:-}"
 
 mkdir -p ${CHROOT_DIR}/${SRC_DIR} ${CHROOT_DIR}/${BUILD_DIR}
 mv * .travis.yml .git ${CHROOT_DIR}/${SRC_DIR} || true
-sudo add-apt-repository --yes 'deb http://ports.ubuntu.com/ubuntu-ports trusty-backports universe' # we need newer qemu-user-static
+sudo add-apt-repository --yes 'deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse' # we need newer qemu-user-static
 sudo apt-get update
 sudo apt-get install --yes --no-install-recommends ${HOST_PACKAGES}
 sudo debootstrap ${FOREIGN} --verbose --no-check-gpg --include=${CHROOT_PACKAGES} --exclude=${CHROOT_PACKAGES_EXCLUDE} --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
