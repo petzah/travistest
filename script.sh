d5a3eb2fabc03a27d22aff9aff6c74a809bcead0
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-20 17:27:51 +0000

    test26

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -xeu
 
-TRAVIS_DEBIAN_TARGET_ARCH="i386"
+TRAVIS_DEBIAN_TARGET_ARCH="armhf"
 TRAVIS_DEBIAN_SUITE="sid"
 TRAVIS_DEBIAN_MIRROR="${TRAVIS_DEBIAN_MIRROR:-http://httpredir.debian.org/debian/}"
+TRAVIS_DEBIAN_GIT_BUILDPACKAGE_OPTIONS="${TRAVIS_DEBIAN_GIT_BUILDPACKAGE_OPTIONS:-}"
 
 
 HOST_PACKAGES="debootstrap qemu-user-static binfmt-support sbuild"
 CHROOT_DIR="$(pwd)/chroot"
 SRC_DIR="/src"
+BUILD_DIR="/build"
 CHROOT_PACKAGES="fakeroot,build-essential,locales"
 CHROOT_PACKAGES_EXCLUDE="init,systemd-sysv"
 
@@ -17,7 +19,7 @@ then
     FOREIGN="--foreign"
 fi
 
-mkdir -p ${CHROOT_DIR}/${SRC_DIR}
+mkdir -p ${CHROOT_DIR}/${SRC_DIR} ${CHROOT_DIR}/${BUILD_DIR}
 mv * .travis.yml .git ${CHROOT_DIR}/${SRC_DIR} || true
 sudo apt-get install --yes --no-install-recommends ${HOST_PACKAGES}
 sudo debootstrap ${FOREIGN} --verbose --no-check-gpg --include=${CHROOT_PACKAGES} --exclude=${CHROOT_PACKAGES_EXCLUDE} --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
@@ -30,8 +32,9 @@ sudo sbuild-createchroot --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${FOREIGN} --setup-
 sudo chroot ${CHROOT_DIR} /bin/bash -x <<EOF
 apt-get update 
 apt-get --yes dist-upgrade
-apt-get --yes install devscripts pkg-config
+apt-get --yes install devscripts pkg-config gbp-buildpackage
 mk-build-deps --host-arch ${TRAVIS_DEBIAN_TARGET_ARCH} --install --remove --tool 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' ${SRC_DIR}/debian/control
 cd ${SRC_DIR}
-make
+gbp buildpackage ${TRAVIS_DEBIAN_GIT_BUILDPACKAGE_OPTIONS} --git-ignore-branch --git-export-dir=${BUILD_DIR} --git-builder='debuild -i -I -uc -us -sa'
+ls -l ${BUILD_DIR}
 EOF
