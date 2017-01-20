8c9e5112af0559ef5716654c841818b59f68456f
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-20 14:56:54 +0000

    test23

--- script.sh
+++ script.sh
@@ -26,7 +26,12 @@ sudo chroot ${CHROOT_DIR} ./debootstrap/debootstrap --second-stage
 #sudo echo "en_US.UTF-8 UTF-8" >> ${CHROOT_DIR}/etc/locale.gen
 #sudo chroot ${CHROOT_DIR} /usr/sbin/locale-gen
 sudo sbuild-createchroot --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${FOREIGN} --setup-only ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
-sudo chroot ${CHROOT_DIR} apt-get update 
-sudo chroot ${CHROOT_DIR} apt-get --yes dist-upgrade
-sudo chroot ${CHROOT_DIR} apt-get --yes install devscripts
-sudo chroot ${CHROOT_DIR} mk-build-deps --host-arch ${TRAVIS_DEBIAN_TARGET_ARCH} --install --remove --tool 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' ${SRC_DIR}/debian/control
+
+sudo chroot ${CHROOT_DIR} /bin/bash -x <<'EOF'
+apt-get update 
+apt-get --yes dist-upgrade
+apt-get --yes install devscripts
+mk-build-deps --host-arch ${TRAVIS_DEBIAN_TARGET_ARCH} --install --remove --tool 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' ${SRC_DIR}/debian/control
+cd ${SRC_DIR}
+make
+EOF
