2a7a302e5c54d16688e5bf7051dfd4226a9210db
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-20 19:17:17 +0000

    test34

--- script.sh
+++ script.sh
@@ -19,9 +19,10 @@ then
 fi
 FOREIGN="${FOREIGN:-}"
 
-add-apt-repository --update --yes 'deb http://ports.ubuntu.com/ubuntu-ports trusty-backports universe'
 mkdir -p ${CHROOT_DIR}/${SRC_DIR} ${CHROOT_DIR}/${BUILD_DIR}
 mv * .travis.yml .git ${CHROOT_DIR}/${SRC_DIR} || true
+sudo add-apt-repository --yes 'deb http://ports.ubuntu.com/ubuntu-ports trusty-backports universe' # we need newer qemu-user-static
+sudo apt-get update
 sudo apt-get install --yes --no-install-recommends ${HOST_PACKAGES}
 sudo debootstrap ${FOREIGN} --verbose --no-check-gpg --include=${CHROOT_PACKAGES} --exclude=${CHROOT_PACKAGES_EXCLUDE} --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
 sudo cp /usr/bin/qemu-$(dpkg-architecture -a${TRAVIS_DEBIAN_TARGET_ARCH} -qDEB_HOST_GNU_CPU)-static ${CHROOT_DIR}/usr/bin/
