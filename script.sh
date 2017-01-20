77e330c27f6ea0ac327dc7a297b9cc0a5970169b
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-20 17:52:43 +0000

    test27

--- script.sh
+++ script.sh
@@ -23,7 +23,7 @@ mkdir -p ${CHROOT_DIR}/${SRC_DIR} ${CHROOT_DIR}/${BUILD_DIR}
 mv * .travis.yml .git ${CHROOT_DIR}/${SRC_DIR} || true
 sudo apt-get install --yes --no-install-recommends ${HOST_PACKAGES}
 sudo debootstrap ${FOREIGN} --verbose --no-check-gpg --include=${CHROOT_PACKAGES} --exclude=${CHROOT_PACKAGES_EXCLUDE} --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
-sudo cp /usr/bin/qemu-${TRAVIS_DEBIAN_TARGET_ARCH}-static ${CHROOT_DIR}/usr/bin/
+sudo cp /usr/bin/qemu-$(dpkg-architecture -a${TRAVIS_DEBIAN_TARGET_ARCH} -qDEB_HOST_ARCH_CPU)-static ${CHROOT_DIR}/usr/bin/
 sudo chroot ${CHROOT_DIR} ./debootstrap/debootstrap --second-stage
 #sudo echo "en_US.UTF-8 UTF-8" >> ${CHROOT_DIR}/etc/locale.gen
 #sudo chroot ${CHROOT_DIR} /usr/sbin/locale-gen
