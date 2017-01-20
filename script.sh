a32e64c05e6201b0c3d2d0d5fd196a0ad9bae7c6
Peter Zahradnik <peter.zahradnik@znik.sk>
2017-01-20 21:13:10 +0000

    test41

--- script.sh
+++ script.sh
@@ -21,8 +21,8 @@ FOREIGN="${FOREIGN:-}"
 
 mkdir -p ${CHROOT_DIR}/${SRC_DIR} ${CHROOT_DIR}/${BUILD_DIR}
 mv * .travis.yml .git ${CHROOT_DIR}/${SRC_DIR} || true
-sudo add-apt-repository --yes 'deb http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse' # we need newer qemu-user-static
-sudo apt-get update
+#sudo add-apt-repository --yes 'deb http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse' # we need newer qemu-user-static
+#sudo apt-get update
 sudo apt-get install --yes --no-install-recommends ${HOST_PACKAGES}
 sudo debootstrap ${FOREIGN} --verbose --no-check-gpg --include=${CHROOT_PACKAGES} --exclude=${CHROOT_PACKAGES_EXCLUDE} --arch=${TRAVIS_DEBIAN_TARGET_ARCH} ${TRAVIS_DEBIAN_SUITE} ${CHROOT_DIR} ${TRAVIS_DEBIAN_MIRROR}
 if [ ! -z "${FOREIGN}" ]
